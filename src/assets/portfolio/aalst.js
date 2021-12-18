2a0dea22f9406ff966cf4ddd5b635a311e87bc34
StephanieHeyrman <stephanie.heyrman@ugent.be>
2021-12-18 18:02:26 +0100

    removed unused images

--- src/assets/portfolio/aalst.js
+++
@@ -1,7 +0,0 @@
-import aalst1 from './aalst1.png'
-import aalst2 from './aalst2.png'
-import aalst3 from './aalst3.png'
-import aalst4 from './aalst4.png'
-
-
-export {aalst1, aalst2, aalst3, aalst4};
